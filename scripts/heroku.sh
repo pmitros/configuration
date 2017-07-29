176601c69914ce17b42c9338972a278b42ea0b94
Piotr Mitros <piotr@mitros.org>
2017-07-29 06:16:14 -0400

    xcftools

--- scripts/heroku.sh
+++ scripts/heroku.sh
@@ -1,1 +1,6 @@
 wget -O- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+echo heroku login
+echo git clone https://github.com/heroku/node-js-getting-started.git
+echo cd node-js-getting-started
+echo heroku create
+echo git push heroku master
